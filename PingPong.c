ac622a0efc4a11b177dfb222b2bad03ce07eeaa9
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 18:57:48 -0300

    Feat: Structs

--- PingPong.c
+++ PingPong.c
@@ -4,6 +4,31 @@
 #include <conio.h>
 #include <windows.h>
 
+typedef enum {
+    STOP = 0, LEFT, UPLEFT, DOWNLEFT, RIGHT, UPRIGHT, DOWNRIGHT
+} eDir;
+
+typedef struct {
+    int x, y;
+    int originalX, originalY;
+    eDir direction;
+} Ball;
+
+typedef struct {
+    int x, y;
+    int originalX, originalY;
+} Paddle;
+
+typedef struct {
+    int width, height;
+    int score1, score2;
+    char up1, down1, up2, down2;
+    int quit;
+    Ball ball;
+    Paddle player1;
+    Paddle player2;
+} GameManager;
+
 int main(){
 
     return 0;
