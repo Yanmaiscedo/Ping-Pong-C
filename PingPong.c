a576f6140d879ac9085e93f2f6a10c73e82573f4
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 18:58:58 -0300

    Feat: Draw function

--- PingPong.c
+++ PingPong.c
@@ -29,6 +29,26 @@ typedef struct {
     Paddle player2;
 } GameManager;
 
+
+void Draw(GameManager *gm) {
+    system("cls");
+    for (int i = 0; i < gm->width + 2; i++) printf("#");
+    printf("\n");
+    
+    for (int i = 0; i < gm->height; i++) {
+        for (int j = 0; j < gm->width; j++) {
+            if (j == 0) printf("#");
+            else if (gm->ball.x == j && gm->ball.y == i) printf("O");
+            else if (gm->player1.x == j && (gm->player1.y <= i && i < gm->player1.y + 4)) printf("|");
+            else if (gm->player2.x == j && (gm->player2.y <= i && i < gm->player2.y + 4)) printf("|");
+            else printf(" ");
+            if (j == gm->width - 1) printf("#");
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < gm->width + 2; i++) printf("#");
+    printf("\nScore 1: %d    Score 2: %d\n", gm->score1, gm->score2);
+}
 int main(){
 
     return 0;
